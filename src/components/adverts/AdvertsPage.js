cdec020c3c194049da8af5861515174bf1bf6b42
Angel Sainero Villar <angelsainero@gmail.com>
2023-04-30 16:34:43 +0200

    feat add filters to advertpage

--- src/components/adverts/AdvertsPage.js
+++ src/components/adverts/AdvertsPage.js
@@ -7,7 +7,9 @@ import { Link } from "react-router-dom";
 const AdvertsPage = () => {
   //creamos estado para validar la carga
   const [isLoading, setIsLoading] = useState(true);
-
+  const [query, setQuery] = useState("");
+  const [minprice, setMinprice] = useState("")
+  const [maxprice, setMaxprice] = useState("100000")
   //se inicia el use state con un array vacio
   const [adverts, setAdverts] = useState([]);
 
@@ -19,6 +21,12 @@ const AdvertsPage = () => {
     });
   }, []);
 
+  const filter = adverts.filter((advert) => (
+    advert.name.toUpperCase().startsWith(query.toUpperCase()) && advert.price > minprice && advert.price < maxprice));
+
+
+  
+
   return (
     <Layout title="Anuncios">
       {isLoading ? (
@@ -26,18 +34,57 @@ const AdvertsPage = () => {
       ) : (
         <div className="advertsPage">
           {!!adverts.length ? (
-            <ul>
-              {adverts.map((advert) => (               
-                <li key={advert.id}>
-                 <div>  
-                  <img src={advert.photo} height="30px" wheight="30px"></img>                
-                  <Link to={`/adverts/${advert.id}`}>
-                    Producto: {advert.name}, Precio: {advert.price}, Venta: {advert.sale.toString()}, Categoría: {advert.tags}
-                  </Link>
-                 </div>
-                </li>
-              ))}
-            </ul>
+            <>
+              <div>
+                <label>
+                  Filtro nombre
+                  <input
+                    type="text"
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                  ></input>
+                </label>
+              </div>
+              <div>
+                <label>
+                  Precio mínimo
+                  <input
+                    type="text"
+                    value={minprice}
+                    onChange={(event2) => setMinprice(event2.target.value)}
+                  ></input>
+                  
+                </label>
+              </div>
+              <div>
+                <label>
+                  Precio maximo
+                  <input
+                    type="text"
+                    value={maxprice}
+                    onChange={(event3) => setMaxprice(event3.target.value)}                    
+                  ></input>
+                  
+                </label>
+              </div>
+              <ul>
+                {filter.map((advert) => (
+                  <li key={advert.id}>
+                    <div>
+                      <img
+                        src={advert.photo}
+                        height="30px"
+                        wheight="30px"
+                      ></img>
+                      <Link to={`/adverts/${advert.id}`}>
+                        Producto: {advert.name}, Precio: {advert.price}, Venta:{" "}
+                        {advert.sale.toString()}, Categoría: {advert.tags}
+                      </Link>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </>
           ) : (
             <Button as={Link} variant="relleno" to="/adverts/new">
               {" "}
